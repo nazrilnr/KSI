1e970253d2a4af8cfc0cc5c5f26428183398193a
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2026-06-10 17:49:36 +0700

    fix search nik

--- search.php
+++ search.php
@@ -11,6 +11,7 @@ $result = mysqli_stmt_get_result($stmt);
 
 echo "<h1>Hasil Pencarian Pasien:</h1>";
 while($row = mysqli_fetch_assoc($result)) {
-    echo "Nama: " . htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . " | NIK: " . htmlspecialchars($row['nik'], ENT_QUOTES, 'UTF-8') . "<br>";
+    echo "Nama: " . htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . " 
+    | NIK: " . htmlspecialchars($row['nik'], ENT_QUOTES, 'UTF-8') . "<br>";
 }
 ?>
