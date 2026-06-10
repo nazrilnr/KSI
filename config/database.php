4d7ca01f7ddb51606524b2e1c0f632b77af19d7a
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2026-06-10 16:42:24 +0700

    Set up connect database

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
-$pass = "mysql";
+$pass = "root";
 $db   = "meditrust_db";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, 8889);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
