4623ec2c9f38393a3aa83a08f3261c3376e284c8
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2026-06-10 18:04:05 +0700

    setup sql

--- db/setup.sql
+++ db/setup.sql
@@ -4,18 +4,18 @@ USE meditrust_db;
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50),
-    password VARCHAR(255), -- Celah: Menggunakan MD5 (Broken Auth)
+    password VARCHAR(255),
     role ENUM('admin', 'dokter')
 );
 
 CREATE TABLE patients (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100),
-    nik VARCHAR(20),      -- Celah: Plaintext (Sensitive Data Exposure)
-    diagnosis TEXT,       -- Celah: Potensi Stored XSS
+    nik VARCHAR(20),
+    diagnosis TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Seed data untuk testing mahasiswa
-INSERT INTO users (username, password, role) VALUES ('dr_ade', md5('password123'), 'dokter');
+INSERT INTO users (username, password, role) VALUES ('dr_ade', '$2y$12$WUz1nQcaRfZms.yqYQGB0OayAlqpswva9TFgJMUtnvlMgltO4SC1y', 'dokter');
 INSERT INTO patients (name, nik, diagnosis) VALUES ('Budi Santoso', '3515010101010001', 'Flu Burung');
